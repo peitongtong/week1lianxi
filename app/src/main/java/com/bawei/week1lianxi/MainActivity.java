6ff993c79726eff4040869d7fdcbd6947e8b971c
peitongtong <1633486096@qq.com>
2019-12-02 15:32:13 +0800

    第一次提交

---
+++ app/src/main/java/com/bawei/week1lianxi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bawei.week1lianxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
